fb210e68b13d1cdf9d3d128cf0b9b6e83d3f969d
Roon ルーン <roonmoonlight@outlook.com>
2023-11-13 08:53:02 +0000

    Unison64.exe
    Starting Unison64 OS 2023.11...

--- tf2/server/tf/addons/sourcemod/plugins/plugins.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BINDIR=$(dirname "$(readlink -fn "$0")")
-cd "$BINDIR"
-mv -ft disabled {basevotes,funcommands,funvotes,nextmap,playercommands,sounds}.smx
--- tf2/start.sh
+++
@@ -1,1 +0,0 @@
-server/srcds_run -console -reuse -game tf +sv_pure 1 +maxplayers 32 +sv_setsteamaccount $TF2_TOKEN +map cp_dustbowl
